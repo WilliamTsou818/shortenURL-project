d11c3b90ed1bbebb5eb17f9d06a5170d617468f3
WilliamTsou818 <cccc1589@gmail.com>
2021-06-11 12:50:23 +0800

    feat: add url seeder data

---
+++ models/seeds/urlSeeder.js
@@ -0,0 +1,21 @@
+const db = require('../../config/mongoose')
+const Url = require('../url')
+
+const data = [
+  {
+    originURL: 'https://forum.gamer.com.tw/C.php?bsn=197&snA=24652&tnum=2',
+    shortenURL: 'http://localhost/q483a',
+    urlCode: 'q483a'
+  },
+  {
+    originURL: 'https://www.youtube.com/',
+    shortenURL: 'http://localhost/p21ad',
+    urlCode: 'p21ad'
+  }
+]
+
+db.once('open', () => {
+  Url.create(data)
+    .then(() => console.log('Add Url seeder!'))
+    .catch(err => console.error(err))
+})
